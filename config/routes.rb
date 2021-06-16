04ec992f02dc0a8f35b7179944b20b870af21975
OM Miles <miles@ommiles.com>
2021-06-16 13:48:12 -0700

    commit tree 7848e5043119e81cbd6f6138adf56da71f3255c9

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,18 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # enables us to browse the stats_controller index action
+  # get '/stats', to: 'stats#index'
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
+
+  # In routing, scope allows us to prefix a block of routes under one grouping.
+  scope '/admin', module: 'admin' do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
